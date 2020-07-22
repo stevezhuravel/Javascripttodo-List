77604c2f402825f079615d4199db47c7b5d16916
Stevezhuravel <Stevezhuravel@gmail.com>
2020-07-22 19:45:12 -0400

    made changes to javascript file

--- list.js
+++ list.js
@@ -1,17 +1,46 @@
 var todos = ["Buy New Tutle"];
-var input = prompt("What would you like to do");
-while(input !== "quit"){
 
+var input = prompt("What would you like to do");
+
+while(input !== "quit"){
 // handle input
 if(input === "list"){
-    console.log(todos);
+listTodos();
 }else if(input === "new"){
-    // ask for new todo list
-    var askNewTodo = prompt("Enter tnew todo");
-    // add to lst
-    todos.push(askNewTodo);
+addTodo();
+} else if(input === "delete"){
+deleteTodo();
 }
+// 
 // ask input again
+// prompts the user what you like to do next
 input = prompt("What would you like to do");
 } 
-console.log("Ok, you quit the app")
+// logs to the console that you quit the app
+console.log("Ok, you quit the app");
+function listTodos(){
+    console.log("******************")
+    todos.forEach(function(todos , i){
+        console.log(i + ": " + todos);
+    });
+    console.log("******************")
+}
+function addTodo(){
+    // ask for new todo list
+    // ask the user to enter new todo list
+    var askNewTodo = prompt("Enter new todo");
+    // add to lst
+    todos.push(askNewTodo);
+    console.log("Added todo");
+}
+function deleteTodo(){
+    // ask of index to be deleted 
+var index = prompt("Enter index of todo to delete")
+//  delete that todo
+// splice()
+// splice makes a cut
+// the first item is where we want to make the cut 
+// the second item is how many items we want to delete
+todos.splice(index, 1)
+console.log("Deleted todo")
+}
